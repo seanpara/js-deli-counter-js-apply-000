f03b742f4d44b993c8b00babb51693c7db0623a3
Sean Logan Para <spara77@gmail.com>
2018-07-16 17:33:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var katzDeliLine = []
+
+function takeANumber(line, name) { 
+  return `Welcome, ${name}. You are number ${line.push(name)} in line.`
+}
+
+function nowServing(line) {
+   if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+    
+  }
+  return `Currently serving ${line.splice(0,1)}.`
+ 
+}
+
+function currentLine(line){
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  
+  var namesString = "The line is currently: "
+
+  for (var i= 0; i < line.length; i++) {
+    namesString += `${i+1}. ${line[i]}`
+    if (i < line.length - 1) {
+      namesString += ", "
+    }
+  } 
+  return namesString
+}
