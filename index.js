5e0fb0971d4b7f3bf0dbfc96561d19083d28fa76
Sean Logan Para <spara77@gmail.com>
2018-07-16 17:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(line) {
 function currentLine(line){
   if (line.length === 0) {
     return "The line is currently empty."
-  }
+    }
   
   var namesString = "The line is currently: "
 
